a7466170de1c0dd90f60d550c3e8c069e60b0e57
mahikaarzare <mahikaarzare786@gmail.com>
2020-01-28 15:35:54 +0530

    first commit Mentioned file added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormControl,FormBuilder,NgForm,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mentioned';
+  signupForm:FormGroup;
+  FirstName:string="";
+  LastName:string="";
+  Email:string="";
+  Password:string="";
+
+
+  constructor(private frmbuilder:FormBuilder){
+    this.signupForm=frmbuilder.group({
+      fname:['',Validators.required],
+      lname:['',Validators.required,Validators.maxLength(10)],
+      EmailId:['',Validators.required,Validators.email],
+      userpassword:['',Validators.required]
+    });
+  }
+  ngOnInit(){
+
+  }
+  PostData(signupForm:any)
+  {
+    this.FirstName=signupForm.controls.fname.value;
+    this.LastName=signupForm.controls.lname.value;
+    this.Email=signupForm.controls.EmailId.value;
+    console.log(this.FirstName);
+
+     console.log(signupForm.controls);
+  }
 }
